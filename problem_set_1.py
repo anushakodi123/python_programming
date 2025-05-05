0467b3a1867df8c7e57450b2472ba275ccd2f379
anushakodi123 <anushakodi01234@gmail.com>
2025-05-05 20:59:20 +0530

    Problem Set 1

---
+++ problem_set_1.py
@@ -0,0 +1,32 @@
+class JobProfessional:
+    def __init__(self, yearly_income, portion_saved, cost_of_dream_home, portion_down_payment, r):
+        self.amount_saved = 0
+        self.yearly_income = yearly_income
+        self.portion_saved = portion_saved
+        self.cost_of_dream_home = cost_of_dream_home
+        self.portion_down_payment = portion_down_payment
+        self.r = r
+
+    def calculate_no_of_months(self):
+        months = 0
+        to_be_paid_for_house = self.portion_down_payment * self.cost_of_dream_home
+        monthly_income = self.yearly_income / 12
+
+        while self.amount_saved < to_be_paid_for_house:
+            interest = self.amount_saved * (self.r / 12)
+            monthly_saving = self.portion_saved * monthly_income
+            self.amount_saved += interest + monthly_saving
+            months += 1
+
+        return months
+
+
+yearly_salary = float(input("Enter your yearly salary: "))
+portion_saved = float(input("Enter the percent of your salary to save, as a decimal: "))
+cost_of_dream_home = float(input("Enter the cost of your dream home: "))
+
+portion_down_payment = 0.25
+r = 0.05
+
+j = JobProfessional(yearly_salary, portion_saved, cost_of_dream_home, portion_down_payment, r)
+print("Number of months:", j.calculate_no_of_months())
