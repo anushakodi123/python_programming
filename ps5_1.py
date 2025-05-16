a67d0029e82cd6fe958fa2720b4610278345a0c6
anushakodi123 <anushakodi01234@gmail.com>
2025-05-16 12:11:26 +0530

    adding assignment 5

--- ps5_1.py
+++ ps5_1.py
@@ -1,4 +1,38 @@
-from PIL import Image
+from PIL import Image, ImageFont, ImageDraw
+
+def make_matrix(color):
+    matrices = {
+        'red': [
+            [0.56667, 0.43333, 0.00000],
+            [0.55833, 0.44167, 0.00000],
+            [0.00000, 0.24167, 0.75833]
+        ],
+        'green': [
+            [0.62500, 0.37500, 0.00000],
+            [0.70000, 0.30000, 0.00000],
+            [0.00000, 0.30000, 0.70000]
+        ],
+        'blue': [
+            [0.95000, 0.05000, 0.00000],
+            [0.00000, 0.43333, 0.56667],
+            [0.00000, 0.47500, 0.52500]
+        ]
+    }
+    return matrices[color]
+
+def matrix_multiply(matrix, vector):
+    return [sum(matrix[i][j] * vector[j] for j in range(3)) for i in range(3)]
+
+def extract_end_bits(num_end_bits, pixel):
+    if isinstance(pixel, tuple):
+        return tuple(i & ((1 << num_end_bits) - 1) for i in pixel)
+    else:
+        return pixel & ((1 << num_end_bits) - 1)
+
+def pix_to_img(pixel_list, size, mode):
+    img = Image.new(mode, size)
+    img.putdata(pixel_list)
+    return img
 
 class ImageProcessor:
     def __init__(self, filename):
@@ -15,65 +49,68 @@ class ImageProcessor:
     def get_size(self):
         return self.image.size
     
-    def pix_to_img(pixel_list, size, mode):
-        img = Image.new(mode, size)
-        img.putdata(pixel_list)
+    def filter(self, color):
+        if color is None:
+            return self.pixels
 
-        return img
-    
-    def make_matrix(color):
-        """Return transformation matrix for a colorblindness type."""
-        matrices = {
-            'red': [
-                [0.56667, 0.43333, 0.00000],
-                [0.55833, 0.44167, 0.00000],
-                [0.00000, 0.24167, 0.75833]
-            ],
-            'green': [
-                [0.62500, 0.37500, 0.00000],
-                [0.70000, 0.30000, 0.00000],
-                [0.00000, 0.30000, 0.70000]
-            ],
-            'blue': [
-                [0.95000, 0.05000, 0.00000],
-                [0.00000, 0.43333, 0.56667],
-                [0.00000, 0.47500, 0.52500]
-            ]
-        }
-        return matrices[color]
-    def matrix_multiply(matrix, vector):
-        result = []
-        for i in range(3):
-            row_sum = 0
-            for j in range(3):
-                row_sum += matrix[i][j] * vector[j]
-            result.append(row_sum)
-        return result
-    
-    def filter(pixels_list, color):
-        if color == None:
-            return pixels_list
-        matrix = ImageProcessor.make_matrix(color)
+        matrix = make_matrix(color)
         transformed = []
-        for pixel in pixels_list:
-            transformed_pixel = ImageProcessor.matrix_multiply(matrix, pixel)
+        for pixel in self.pixels:
+            transformed_pixel = matrix_multiply(matrix, pixel)
             clamped_pixel = tuple(max(0, min(255, int(round(i)))) for i in transformed_pixel)
             transformed.append(clamped_pixel)
 
         return transformed
 
+    def reveal_bw_image(self):
+        return [pix & 1 for pix in self.pixels]
 
+    def reveal_color_image(self):
+        return [extract_end_bits(3, pix) for pix in self.pixels]
 
+    def reveal_image(self):
+        if self.get_mode() in ['1', 'L']:
+            return self.reveal_bw_image()
+        elif self.get_mode() == 'RGB':
+            return self.reveal_color_image()
+        else:
+            raise ValueError(f"Unsupported image mode: {self.get_mode()}")
 
-processor = ImageProcessor('hidden2.bmp')
-pixels = processor.get_pixels()
-print('First 10 pixels: ', pixels[:10])
-print('mode of the image: ', processor.get_mode())
-print('size of the image: ', processor.get_size())
-new_image = ImageProcessor.pix_to_img(pixels, processor.get_size(), processor.get_mode())
+    def draw_kerb(self, kerb):
+        im = self.image.copy()
+        font = ImageFont.truetype("noto-sans-mono.ttf", 40)
+        draw = ImageDraw.Draw(im)
+        draw.text((0, 0), kerb, "white", font=font)
+        idx = self.filename.rfind(".")
+        new_filename = self.filename[:idx] + "_kerb" + self.filename[idx:]
+        im.save(new_filename)
+        return new_filename
 
-new_image.show()
+def main():
+    processor = ImageProcessor('hidden2.bmp')
+    
+    print('First 10 pixels:', processor.get_pixels()[:10])
+    print('Mode:', processor.get_mode())
+    print('Size:', processor.get_size())
 
-pixels = [(120, 200, 150), (30, 60, 90), (255, 100, 50)]
-filtered_pixels = ImageProcessor.filter(pixels, 'red')
-print(filtered_pixels)
+    # Re-create and show image from pixels
+    new_image = pix_to_img(processor.get_pixels(), processor.get_size(), processor.get_mode())
+    new_image.show()
+
+    # Filter example
+    pixels = [(120, 200, 150), (30, 60, 90), (255, 100, 50)]
+    processor.pixels = pixels
+    print('Red filtered:', processor.filter('red'))
+
+    # Bit extraction examples
+    print('Extract 5 bits from 214:', extract_end_bits(5, 214))
+    print('Extract 3 bits from (214, 17, 8):', extract_end_bits(3, (214, 17, 8)))
+
+    # Reveal BW
+    bw_processor = ImageProcessor('hidden1.bmp')
+    print('BW reveal (first 10):', bw_processor.reveal_image()[:10])
+    pixel_list = bw_processor.reveal_image()[:100]
+    print(pix_to_img(pixel_list, (300, 300), 'L'))
+
+if __name__ == '__main__':
+    main()
